1e1fb65387e130d6145d6c76904187c1bf96a349
burenzak <zakharov1anton@gmail.com>
2021-10-03 12:46:34 +0300

    10/3

--- django_proj_1/posts/views.py
+++ django_proj_1/posts/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .forms import PostForm
-
+from .models import Post
 
 # Create your views here.
+
+def detail(request, id):
+    post = get_object_or_404(Post, pk=id)
+    return render(request, "posts/detail.html", {"post": post})
+
+def delete(request, id):
+    post = get_object_or_404(Post, pk=id)
+    post.delete()
+    return redirect("/")
+
+@login_required()
 def new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
